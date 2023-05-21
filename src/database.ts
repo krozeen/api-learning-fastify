8224095b33b509634c1ac100182ab614628a5573
Joao Freitas <joao.freitas@wisereducacao.com>
2023-05-21 17:41:38 -0300

    Configuration enviroments app

--- src/database.ts
+++ src/database.ts
@@ -1,9 +1,10 @@
+import { env } from './env'
 import { Knex, knex as setupKnex } from 'knex'
 
 export const config: Knex.Config = {
   client: 'sqlite',
   connection: {
-    filename: './database/app.db',
+    filename: env.DATABASE_URL,
   },
   useNullAsDefault: true,
   migrations: {
--- src/server.ts
+++ src/server.ts
@@ -1,16 +1,27 @@
+import { env } from './env'
 import fastify from 'fastify'
+// import crypto from 'node:crypto'
 import { knex } from './database'
 
 const app = fastify()
 
 app.get('/', async () => {
-  const tables = await knex('sqlite_schema').select('*')
-  return tables
+  /* const transactions = await knex('transactions')
+    .insert({
+      id: crypto.randomUUID(),
+      title: 'Transação de teste',
+      amount: 1000,
+    })
+    .returning('*') */
+
+  const transactions = await knex('transactions').select('*')
+
+  return transactions
 })
 
 app
   .listen({
-    port: 3333,
+    port: env.PORT,
   })
   .then(() => {
     console.log('HTTP Server Running!')
